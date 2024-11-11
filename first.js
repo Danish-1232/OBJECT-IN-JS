f61bc7b67dc39d33888930780dee9c35d878949f
Danish-Farooqui <danishfarooqui303@gmail.com>
2024-11-11 15:15:41 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,42 @@
+// 1
+
+var book = {
+    Name: "Aao Umra Karen",
+    Title: "Aasan Lafzon Me",
+    Author: "Sualeh Ahqar",
+    YearPublished: 2023,
+};
+console.log(book);
+
+// 2
+
+var student = {
+    Name: "Huzaifa",
+    Age: 19,
+    Class: "12th",
+    Subject: "English",
+    City: "Saharanpur",
+};
+
+console.log(student);
+
+// 3
+
+var movie = {
+    Name: "Sultan",
+    Director: "Karan Johar",
+    ReleaseYear: 2021,
+};
+
+console.log(movie);
+
+// 4
+
+var mobile = {
+    Name: "Samsung",
+    Brand: "S24 Ultra",
+    Price: 89000,
+};
+
+console.log(mobile);
+
